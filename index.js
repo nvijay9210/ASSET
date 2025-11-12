134687fe195b685feba1bb3012623da1f35d263a
nvijay9210 <vijaybeeee2020@gmail.com>
2025-11-12 14:08:45 +0530

    asset-.env

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// require('dotenv').config({ quiet: true }); // ✅ suppress logs
+const {app} = require('./app');
+const dotenv = require('dotenv');
 
-require('dotenv').config();
+const env = process.env.NODE_ENV || 'development';
+
+dotenv.config(); // Load base .env first
+dotenv.config({ path: `.env.${env}` }); // Then env-specific override
+
+const config = require('./Config/Config');
+
+const PORT = process.env.PORT || 5000;
+
+console.log(`Environment: ${env}`);
+console.log(`Using DB Host: ${process.env.ASSET_DB_HOST}`);
+console.log(`Server listening on port: ${PORT}`);
+console.log('Loaded DB User:', process.env.ASSET_DB_USER);
+console.log('Loaded DB Password:', process.env.ASSET_DB_PASS);
+console.log('Loaded DB Host:', process.env.ASSET_DB_HOST);
 
 
-const { app } = require('./app');
-const http = require('http');
-
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`✅ Asset Server running on ${process.env.ASSET_DB_HOST} port ${PORT}`);
 });
